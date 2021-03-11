6b5d4e8aef6be5dc6671b7fb8ad0f4e7c30c05b4
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-11 22:09:32 +1100

    UPDATE Program.cs - rewrite the main meth, complete task

--- 1.1P/HelloWorld/HelloWorld/Program.cs
+++ 1.1P/HelloWorld/HelloWorld/Program.cs
@@ -2,16 +2,47 @@
 
 namespace HelloWorld
 {
-    class MainClass
+    internal static class MainClass
     {
         public static void Main(string[] args)
         {
-
-        Message myMessage;
-        myMessage = new Message("Hello World - from Message Object");
+        
+        String[] message_text = {"Welcome back oh great eud"}
+        while ()    
+        Message[] messages = new Message[4];
+        messages[0] = new Message("Welcome back oh great educator");
+        messages[1] = new Message("What a lovely name");
+        messages[2] = new Message("Great name");
+        messages[3] = new Message("That is a silly name");
+        
+        Message myMessage = new Message("Hello World - from Message Object");
         myMessage.Print();
         
-        Console.ReadLine();
+        Console.WriteLine("Enter name:");
+        string line = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(line))
+        {
+            switch (line)
+            {
+                case "Josh":
+                    messages[0].Print();
+                    break;
+                case "Jack":
+                    messages[1].Print();
+                    break;
+                case "Mitch":
+                    messages[2].Print();
+                    break;
+                default:
+                    messages[3].Print();
+                    break;
+            }             
+        }
+        else
+        {
+            Console.WriteLine("If you don't provide a name, I can't work properly!");
+        }
 
         }
     }
