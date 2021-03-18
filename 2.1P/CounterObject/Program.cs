238c0f772050a60224d92c2d6ac2b69194915c53
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-18 20:26:45 +1100

    CREATE PROJECT 2.1P - boilerplate the Counter Object project

---
+++ 2.1P/CounterObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CounterObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
