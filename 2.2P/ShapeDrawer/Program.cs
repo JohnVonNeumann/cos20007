6ba78ed7511532296b8384bbb7a3da860d5dcd3a
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-22 22:14:41 +1100

    UPDATE Program.cs - add in graphics drawing to the loop

--- 2.2P/ShapeDrawer/Program.cs
+++ 2.2P/ShapeDrawer/Program.cs
@@ -6,11 +6,23 @@ namespace ShapeDrawer
     {
         public static void Main()
         {
+            Shape myShape = new Shape();
             new Window("Shape Drawer", 800, 600);
             do
             {
                 SplashKit.ProcessEvents();
                 SplashKit.ClearScreen();
+                myShape.Draw();
+                if (SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    myShape.X = SplashKit.MouseX();
+                    myShape.Y = SplashKit.MouseY();
+                }
+
+                if (myShape.IsAt(SplashKit.MousePosition()))
+                {
+                    myShape.Color = Color.Aquamarine;
+                }
                 SplashKit.RefreshScreen();
             } while (!SplashKit.WindowCloseRequested("Shape Drawer"));
         }
