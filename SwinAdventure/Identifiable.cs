2a5e6f76df18f751761fc63ebaaec5cad8042175
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-23 22:05:19 +1100

    UPDATE Iden.cs - add basic structure for the file

--- SwinAdventure/Identifiable.cs
+++ SwinAdventure/Identifiable.cs
@@ -1,7 +1,41 @@
+using System.Collections.Generic;
+
 namespace SwinAdventure
 {
     public class Identifiable
     {
-        
+        private List<string> _identifiers;
+
+        public Identifiable(string[] idents)
+        {
+            
+        }
+
+        public bool AreYou(string id)
+        {
+            bool areYou = false;
+
+            return areYou;
+        }
+
+        public string FirstId
+        {
+
+            get
+            {
+                string identifier = "";
+                if (_identifiers.Count != 0)
+                {
+                    identifier = _identifiers[0];
+                }
+
+                return identifier;
+            }
+        }
+
+        public void AddIdentifier(string id)
+        {
+            
+        }
     }
 }
