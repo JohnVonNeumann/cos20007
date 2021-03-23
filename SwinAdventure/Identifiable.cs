569325d728a85b967b6fa658e756c3f4de45f260
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-23 22:28:04 +1100

    UPDATE Iden.cs - add doc comments for class

--- SwinAdventure/Identifiable.cs
+++ SwinAdventure/Identifiable.cs
@@ -2,6 +2,11 @@ using System.Collections.Generic;
 
 namespace SwinAdventure
 {
+    /// <summary>
+    /// class <c> Identifiable </c>
+    /// Players of Swin Adventure need to be able to "identify" a number of things in the game.
+    /// This includes commands the user will perform, items they will interact with, location, paths, etc.
+    /// </summary>
     public class Identifiable
     {
         private List<string> _identifiers;
@@ -11,6 +16,11 @@ namespace SwinAdventure
             
         }
 
+        /// <summary>
+        /// method <c> AreYou </c> checks if the passed in identifier is in the _identifiers
+        /// </summary>
+        /// <param name="id"> identifier used to refer to an identifiable object </param>
+        /// <returns> bool - true if the identifer is in _identifiers </returns>
         public bool AreYou(string id)
         {
             bool areYou = false;
@@ -18,6 +28,10 @@ namespace SwinAdventure
             return areYou;
         }
 
+        /// <summary>
+        /// property <c> FirstId </c> returns the first identifier from _identifiers or an empty string
+        /// <returns> string - the first identifier from _identifiers </returns>
+        /// </summary>
         public string FirstId
         {
 
@@ -33,6 +47,10 @@ namespace SwinAdventure
             }
         }
 
+        /// <summary>
+        /// method <c> AddIdentifier </c> converts the identifier to lowercase and stores it in _identifiers
+        /// </summary>
+        /// <param name="id"> string - the identifier to be added to _identifiers, before lowercase conversion </param>
         public void AddIdentifier(string id)
         {
             
