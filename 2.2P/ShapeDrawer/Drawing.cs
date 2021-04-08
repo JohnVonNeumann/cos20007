0c4dd1929f256f255f13af2bc9f3d29c81b3a7be
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-09 00:25:32 +1000

    CREATE Drawing.cs - add new Drawing class

---
+++ 2.2P/ShapeDrawer/Drawing.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    public class Drawing
+    {
+
+        private readonly List<Shape> _shapes;
+        private readonly Color _background;
+
+        public Drawing(Color background)
+        {
+            _shapes = new List<Shape>();
+            _background = background;
+        }
+
+        public Drawing() : this(Color.White)
+        {
+            
+        }
+
+        public void AddShape(Shape shape)
+        {
+            _shapes.Add(shape);
+        }
+        
+        public Color Background
+        {
+            get
+            {
+                return _background;
+            }
+        }
+
+        public int ShapeCount
+        {
+            get
+            {
+                return _shapes.Count;
+            }
+        }
+
+    }
+}
