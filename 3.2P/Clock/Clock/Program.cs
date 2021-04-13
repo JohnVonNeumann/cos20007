f23250ffe88bab4e2cd3468372f91ed77fa3aaa5
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-13 21:49:45 +1000

    CREATE 3.2P - init solution for Clock project

---
+++ 3.2P/Clock/Clock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
