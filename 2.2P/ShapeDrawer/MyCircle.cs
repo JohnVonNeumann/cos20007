e397da8d55a38ea0bc0f049515bc14460ed2e828
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-15 12:47:41 +1000

    UPDATE MyCircle.cs - add _radius field, constructor and Radius property get/set

--- 2.2P/ShapeDrawer/MyCircle.cs
+++ 2.2P/ShapeDrawer/MyCircle.cs
@@ -2,6 +2,24 @@ namespace ShapeDrawer
 {
     public class MyCircle : Shape
     {
+        private int _radius;
+
+        public MyCircle()
+        {
+            _radius = 50;
+        }
         
+        public int Radius
+        {
+            get
+            {
+                return _radius;
+            }
+
+            set
+            {
+                _radius = value;
+            }
+        }
     }
 }
