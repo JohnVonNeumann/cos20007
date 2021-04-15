868c94e12b832523c6e40de86d0693380991a6b8
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-15 14:18:01 +1000

    UPDATE MyRect/Shape.cs - migrate the height and width props to MyRect

--- 2.2P/ShapeDrawer/MyRectangle.cs
+++ 2.2P/ShapeDrawer/MyRectangle.cs
@@ -3,5 +3,40 @@ namespace ShapeDrawer
     public class MyRectangle : Shape
     {
         private int _width, _height;
+        
+        
+        /// <summary>
+        /// property <c> Width </c>
+        /// Provides access to the Width field through a getter and setter
+        /// </summary>
+        /// <returns> int - the value of _width which represents the width of a shape </returns>
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+
+        /// <summary>
+        /// property <c> Height </c>
+        /// Provides access to the Height field through a getter and setter
+        /// </summary>
+        /// <returns> float - the value of _height which represents the height of a shape </returns>
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+        }
     }
 }
--- 2.2P/ShapeDrawer/Shape.cs
+++ 2.2P/ShapeDrawer/Shape.cs
@@ -63,39 +63,6 @@ namespace ShapeDrawer
             }
         }
 
-        /// <summary>
-        /// property <c> Width </c>
-        /// Provides access to the Width field through a getter and setter
-        /// </summary>
-        /// <returns> int - the value of _width which represents the width of a shape </returns>
-        public int Width
-        {
-            get
-            {
-                return _width;
-            }
-            set
-            {
-                _width = value;
-            }
-        }
-
-        /// <summary>
-        /// property <c> Height </c>
-        /// Provides access to the Height field through a getter and setter
-        /// </summary>
-        /// <returns> float - the value of _height which represents the height of a shape </returns>
-        public int Height
-        {
-            get
-            {
-                return _height;
-            }
-            set
-            {
-                _height = value;
-            }
-        }
 
         /// <summary>
         /// property <c> Color </c>
