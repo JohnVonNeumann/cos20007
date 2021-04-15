9980d326649a75486dc04879226f01fa82357099
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-15 15:03:43 +1000

    UPDATE MyCircle.cs - implement IsAt override method

--- 2.2P/ShapeDrawer/MyCircle.cs
+++ 2.2P/ShapeDrawer/MyCircle.cs
@@ -6,10 +6,12 @@ namespace ShapeDrawer
     {
         private int _radius;
 
-        public MyCircle()
+        public MyCircle(Color colour, int radius)
         {
-            _radius = 50;
+            _radius = radius;
         }
+        
+        public MyCircle() : this(Color.Blue, 50) {}
 
         public override void Draw()
         {
@@ -27,6 +29,24 @@ namespace ShapeDrawer
                 x: X,
                 y: Y,
                 radius: _radius + 2);
+        }
+
+        public override bool IsAt(Point2D pt)
+        {
+            bool pointWithinRange = false;
+            double xMax = X + _radius;
+            double xMin = X - _radius;
+            double yMax = Y + _radius;
+            double yMin = Y - _radius;
+            if ((pt.X > xMin) && (pt.X < xMax))
+            {
+                if ((pt.Y > yMin) && (pt.Y < yMax))
+                {
+                    pointWithinRange = true;
+                }    
+            } 
+                
+            return pointWithinRange;
         }
 
         public int Radius
