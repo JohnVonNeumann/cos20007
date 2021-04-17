50a5fdd1a9f0e7395e9e363e55ae63c41f95c4d8
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-18 01:38:35 +1000

    UPDATE Bag.cs - stub methods prior to testing

--- SwinAdventure/SwinAdventure/Bag.cs
+++ SwinAdventure/SwinAdventure/Bag.cs
@@ -1,7 +1,36 @@
+using System;
+using SwinAdventure.Abstract;
+
 namespace SwinAdventure
 {
     public class Bag
     {
+        private Inventory _inventory;
         
+        public Bag(string[] ids, string name, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameObject Locate(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FullDescription
+        {
+            get
+            {
+                return "FullDescription";
+            }
+        }
+
+        public Inventory Inventory
+        {
+            get
+            {
+                return _inventory;
+            }
+        }
     }
 }
