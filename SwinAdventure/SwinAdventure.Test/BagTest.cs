8bc69abf717aad0021e7e790a28ad595634ec55d
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-18 01:33:32 +1000

    UPDATE BagTest.cs - add setup, imports and fields

--- SwinAdventure/SwinAdventure.Test/BagTest.cs
+++ SwinAdventure/SwinAdventure.Test/BagTest.cs
@@ -1,7 +1,15 @@
+using NUnit.Framework;
+
 namespace SwinAdventure.Test
 {
     public class BagTest
     {
-        
+        private Bag _bag;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bag = new Bag();
+        }
     }
 }
