2c68294b6c5626dea9837b32af64ea4c85ae60cd
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-17 23:57:27 +1000

    UPDATE PlayerTest.cs - create items and put them in inventory via SetUp

--- SwinAdventure/SwinAdventure.Test/PlayerTest.cs
+++ SwinAdventure/SwinAdventure.Test/PlayerTest.cs
@@ -10,6 +10,28 @@ namespace SwinAdventure.Test
         public void Setup()
         {
             _player = new Player("John", "Player 1");
+
+            string[] identsGun = {"Weapon", "BFG", "Rocket Launcher"};
+            Item weapon = new Item(identsGun,
+                "Big Friggin Gun",
+                "The biggest gun available in the game"
+            );
+
+            string[] identsArmour = {"Armour", "Platemail", "Heavy Armour"};
+            Item armour = new Item(identsArmour,
+                "Illidan's Plated Glory",
+                "Tier 9 Epic Plate Mail"
+            );
+
+            string[] identsConsumables = {"Food", "Consumable", "Health Regeneration"};
+            Item food = new Item(identsConsumables,
+                "Kiwifruit Pie",
+                "Heals Player on consumption for 2000 hp over 12 seconds"
+            );
+
+            _player.Inventory.Put(weapon);
+            _player.Inventory.Put(armour);
+            _player.Inventory.Put(food);
         }
 
         [Test]
