28134a642bf9d23b7f77300c2685223ce9fee9f0
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-17 15:38:27 +1000

    UPDATE Inventory.cs - stub the class for testing

--- SwinAdventure/SwinAdventure/Inventory.cs
+++ SwinAdventure/SwinAdventure/Inventory.cs
@@ -1,7 +1,41 @@
+using System;
+using System.Collections.Generic;
+
 namespace SwinAdventure
 {
     public class Inventory
     {
+        private List<Item> _items = new List<Item>();
+        
+        public Inventory(){}
+
+        public bool HasItem(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item Take(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item Fetch(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ItemList
+        {
+            get
+            {
+                return "Item List";
+            }
+        }
         
     }
 }
