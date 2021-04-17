1f0f4c4192cf8e0451586f1c5a4b377187f35046
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-17 20:43:32 +1000

    UPDATE Player.cs - stub fields, methods and properties

--- SwinAdventure/SwinAdventure/Player.cs
+++ SwinAdventure/SwinAdventure/Player.cs
@@ -1,7 +1,36 @@
+using System;
+using SwinAdventure.Abstract;
+
 namespace SwinAdventure
 {
-    public class Player
+    public class Player : GameObject
     {
-        
+        private Inventory _inventory;
+
+        public Player(string name, string description) :
+            base(name, description, new[] {"me", "inventory"})
+        {
+        }
+
+        public GameObject Locate(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return "Full Description";
+            }
+        }
+
+        public Inventory Inventory
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
