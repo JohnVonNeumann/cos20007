a0361846d909b82326b1f555561ee95335ccdf4b
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-17 20:46:25 +1000

    UPDATE PlayerTest.cs - stub tests

--- SwinAdventure/SwinAdventure.Test/PlayerTest.cs
+++ SwinAdventure/SwinAdventure.Test/PlayerTest.cs
@@ -9,5 +9,35 @@ namespace SwinAdventure.Test
         {
             
         }
+
+        [Test]
+        public void TestPlayerIsIdentifiable()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestPlayerLocatesItems()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestPlayerLocatesItself()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestPlayerLocatesNothing()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestPlayerFullDescription()
+        {
+            Assert.Pass();
+        }
     }
 }
