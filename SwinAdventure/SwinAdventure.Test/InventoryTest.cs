2e713e8b4cc0e296e96729ef2c6c70702072898e
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-17 19:03:51 +1000

    UPDATE InvTest.cs - implement test for ItemList and checking return val

--- SwinAdventure/SwinAdventure.Test/InventoryTest.cs
+++ SwinAdventure/SwinAdventure.Test/InventoryTest.cs
@@ -49,7 +49,36 @@ namespace SwinAdventure.Test
         [Test]
         public void TestItemList()
         {
-            Assert.Pass();
+            string[] identsGun = {"Weapon", "BFG", "Rocket Launcher"};
+            Item weapon = new Item(identsGun,
+                "Big Friggin Gun",
+                "The biggest gun available in the game"
+            );
+
+            string[] identsArmour = {"Armour", "Platemail", "Heavy Armour"};
+            Item armour = new Item(identsArmour,
+                "Illidan's Plated Glory",
+                "Tier 9 Epic Plate Mail"
+            );
+
+            string[] identsConsumables = {"Food", "Consumable", "Health Regeneration"};
+            Item food = new Item(identsConsumables,
+                "Kiwifruit Pie",
+                "Heals Player on consumption for 2000 hp over 12 seconds"
+            );
+
+            _inventory.Put(weapon);
+            _inventory.Put(armour);
+            _inventory.Put(food);
+
+            string[] expected =
+            {
+                "Big Friggin Gun - Weapon",
+                "Illidan's Plated Glory - Armour",
+                "Kiwifruit Pie - Food"
+            };
+
+            Assert.AreEqual(expected, _inventory.ItemList);
         }
     }
 }
