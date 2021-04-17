3c74471e4f667a1ddc2802f3704c17a2a7e60efb
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-17 15:47:29 +1000

    UPDATE InventoryTest.cs - setup Test Suite and stub tests

--- SwinAdventure/SwinAdventure.Test/InventoryTest.cs
+++ SwinAdventure/SwinAdventure.Test/InventoryTest.cs
@@ -5,6 +5,40 @@ namespace SwinAdventure.Test
 {
     public class InventoryTest
     {
-        
+        [SetUp]
+        public void Setup()
+        {
+            Inventory inventory = new Inventory();
+        }
+
+        [Test]
+        public void TestFindItem()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestNoItemFind()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestFetchItem()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestTakeItem()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestItemList()
+        {
+            Assert.Pass();
+        }
     }
 }
