2354b281fde92989ef7b165b52a1c734a1f0a49a
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-18 21:43:24 +1000

    UPDATE LCTest.cs - init different objects for testing purposes

--- SwinAdventure/SwinAdventure.Test/LookCommandTest.cs
+++ SwinAdventure/SwinAdventure.Test/LookCommandTest.cs
@@ -5,11 +5,40 @@ namespace SwinAdventure.Test
     public class LookCommandTest
     {
         private LookCommand _lookCommand;
+        private Player _player;
+        private Bag _bag;
+        private Item _weapon;
+        private Item _armour;
+        private Item _food;
 
         [SetUp]
         public void Setup()
         {
             _lookCommand = new LookCommand();
+            _player = new Player("Varg", "The Enflamer");
+            _bag = new Bag(
+                new []{"bag"}, 
+                "Netherweave Bag", 
+                "A hand crafted elven bag."
+                );
+            
+            string[] identsGun = {"Weapon", "BFG", "Rocket Launcher"};
+            _weapon = new Item(identsGun,
+                "Big Friggin Gun",
+                "The biggest gun available in the game"
+            );
+
+            string[] identsArmour = {"Armour", "Platemail", "Heavy Armour"};
+            _armour = new Item(identsArmour,
+                "Illidan's Plated Glory",
+                "Tier 9 Epic Plate Mail"
+            );
+
+            string[] identsConsumables = {"Food", "Consumable", "Health Regeneration"};
+            _food = new Item(identsConsumables,
+                "Kiwifruit Pie",
+                "Heals Player on consumption for 2000 hp over 12 seconds"
+            );
         }
 
         [Test]
