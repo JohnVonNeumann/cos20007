65575212d3fd684505eefb0de205bafebc43a83c
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-18 16:16:37 +1000

    UPDATE BagTest.cs - create items in Setup for further testing

--- SwinAdventure/SwinAdventure.Test/BagTest.cs
+++ SwinAdventure/SwinAdventure.Test/BagTest.cs
@@ -5,12 +5,33 @@ namespace SwinAdventure.Test
     public class BagTest
     {
         private Bag _bag;
+        private Item _weapon;
+        private Item _armour;
+        private Item _food;
 
         [SetUp]
         public void Setup()
         {
             string[] idents = new[] {"bag", "backpack"};
             _bag = new Bag(idents, "Netherweave Bag", "A hand crafted elven bag.");
+            string[] identsGun = {"Weapon", "BFG", "Rocket Launcher"};
+            _weapon = new Item(identsGun,
+                "Big Friggin Gun",
+                "The biggest gun available in the game"
+            );
+
+            string[] identsArmour = {"Armour", "Platemail", "Heavy Armour"};
+            _armour = new Item(identsArmour,
+                "Illidan's Plated Glory",
+                "Tier 9 Epic Plate Mail"
+            );
+
+            string[] identsConsumables = {"Food", "Consumable", "Health Regeneration"};
+            _food = new Item(identsConsumables,
+                "Kiwifruit Pie",
+                "Heals Player on consumption for 2000 hp over 12 seconds"
+            );
+
         }
 
         [Test]
