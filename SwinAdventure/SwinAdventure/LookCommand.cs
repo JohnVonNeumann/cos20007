6abafe0a98080bd68cb0ebbfc89fa82e87c338f6
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-19 00:41:58 +1000

    UPDATE LookCommand.cs - add all validation rules to Execute()

--- SwinAdventure/SwinAdventure/LookCommand.cs
+++ SwinAdventure/SwinAdventure/LookCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using SwinAdventure.Abstract;
 
 namespace SwinAdventure
@@ -11,9 +12,27 @@ namespace SwinAdventure
 
         public override string Execute(Player p, string[] text)
         {
-            if (text.Length != (3 | 5))
+            if (!new[] {3, 5}.Contains(text.Length))
             {
                 return "I don't know how to look like that";
+            }
+
+            if (text[0] != "look")
+            {
+                return "Error in look input";
+            }
+
+            if (text[1] != "at")
+            {
+                return "What do you want to look at?";
+            }
+
+            if (text.Length == 5)
+            {
+                if (text[3] != "in")
+                {
+                    return "What do you want to look in?";
+                }
             }
 
             return "Success";
