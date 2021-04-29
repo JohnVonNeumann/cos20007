1083c9b2e25b4584665e8f680206763cee41c390
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-30 02:10:20 +1000

    UPDATE Program.cs - add test to main to show the program working

--- 8.1P/Library/Library/Program.cs
+++ 8.1P/Library/Library/Program.cs
@@ -6,7 +6,23 @@ namespace Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Library library = new Library("Victorian State Library");
+
+            Book darkAlliance = new Book("Dark Alliance", "Gary Webb", "12345");
+            Book marketsSolved = new Book("The Man Who Solved the Markets", "Jim Simons", "98765");
+            darkAlliance.OnLoan = true;
+
+            Game fable = new Game("Fable", "Lionhead Studios", "M");
+            Game grandTheftAuto = new Game("Grand Theft Auto 4", "Rockstar Games", "R");
+            grandTheftAuto.OnLoan = true;
+
+            library.AddResource(darkAlliance);
+            library.AddResource(marketsSolved);
+            library.AddResource(fable);
+            library.AddResource(grandTheftAuto);
+
+            Console.WriteLine("Is Dark Alliance Available for Loan? " + library.HasResource("Dark Alliance"));
+            Console.WriteLine("Is Fable Available for Loan? " + library.HasResource("Fable"));
         }
     }
 }
