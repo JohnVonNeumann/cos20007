39d4b7f293f7ee88303ef13bdc4258bf63d1b342
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-30 00:05:22 +1000

    UPDATE Library.cs - stub out the Library class

--- 8.1P/Library/Library/Library.cs
+++ 8.1P/Library/Library/Library.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
 namespace Library
 {
     public class Library
     {
+        private List<Book> _books;
+        private List<Game> _games;
+        
+        public Library(string name) {}
+        
+        public void AddBook(Book b) {}
+        
+        public void AddGame(Game g) {}
+
+        public bool HasBook(string name)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool HasGame(string name)
+        {
+            throw new NotImplementedException();
+        }
         
     }
 }
