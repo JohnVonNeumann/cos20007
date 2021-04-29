ef09aad00c8bbee22fdf798c14c153f584671087
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-29 23:50:01 +1000

    UPDATE Game.cs - stub out the Game class

--- 8.1P/Library/Library/Game.cs
+++ 8.1P/Library/Library/Game.cs
@@ -2,6 +2,44 @@ namespace Library
 {
     public class Game
     {
+        private string _name;
+        private string _developer;
+        private string _contentRating;
+        private bool _onLoan;
+        
+        public Game(string name, string creator, string rating) {}
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public string Developer
+        {
+            get
+            {
+                return _developer;
+            }
+        }
+
+        public string ContentRating
+        {
+            get
+            {
+                return _contentRating;
+            }
+        }
+
+        public bool OnLoan
+        {
+            get
+            {
+                return _onLoan;
+            }
+        }
         
     }
 }
