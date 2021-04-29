c2bc053754b49d20ef9e8017d39533aa77412419
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-30 01:44:17 +1000

    UPDATE LibRes.cs - implement the class

--- 8.1P/Library/Library/Abstract/LibraryResource.cs
+++ 8.1P/Library/Library/Abstract/LibraryResource.cs
@@ -2,6 +2,34 @@ namespace Library.Abstract
 {
     public abstract class LibraryResource
     {
-        
+        private string _name;
+        private bool _onLoan;
+
+        public LibraryResource(string name)
+        {
+            _name = name;
+            _onLoan = false;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public bool OnLoan
+        {
+            get
+            {
+                return _onLoan;
+            }
+
+            set
+            {
+                _onLoan = value;
+            }
+        }
     }
 }
