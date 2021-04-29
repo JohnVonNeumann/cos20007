540cef92c0508ba250f7dd5faa7812bd5b7758f0
JohnVonNeumann <louiswillcock@gmail.com>
2021-04-29 23:59:15 +1000

    UPDATE Book.cs - stub out Book class

--- 8.1P/Library/Library/Book.cs
+++ 8.1P/Library/Library/Book.cs
@@ -2,6 +2,51 @@ namespace Library
 {
     public class Book
     {
-        
+        private string _name;
+        private string _author;
+        private string _isbn;
+        private bool _onLoan;
+
+        public Book(string name, string creator, string isbn)
+        {
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public string Creator
+        {
+            get
+            {
+                return _author;
+            }
+        }
+
+        public string ISBN
+        {
+            get
+            {
+                return _isbn;
+            }
+        }
+
+        public bool OnLoan
+        {
+            get
+            {
+                return _onLoan;
+            }
+
+            set
+            {
+                _onLoan = value;
+            }
+        }
+
     }
 }
