cab4b6569378c2306f9e53a1f75e5a672c30da0c
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-03 00:22:51 +1000

    CREATE 10.1P - start py impl of clock program

---
+++ 10.1P/main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    print("Hello")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
