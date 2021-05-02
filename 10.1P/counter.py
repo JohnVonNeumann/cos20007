1cbc0160a5b476220747c46fdfa81db6d5c0f2a9
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-03 00:23:06 +1000

    CREATE counter.py - create the counter class

---
+++ 10.1P/counter.py
@@ -0,0 +1,25 @@
+class Counter(object):
+
+    def __init__(self, name):
+        self._count = 0
+        self._name = name
+
+    # Methods
+    def increment(self):
+        self._count += 1
+
+    def reset(self):
+        self._count = 0
+
+    # Properties
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def ticks(self) -> int:
+        return self._count
