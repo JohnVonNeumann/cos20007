44f0f8d087e5c771f523d8985e09b857a600d895
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-03 00:57:09 +1000

    UPDATE main.py - rewrite to test and show off the clock

--- 10.1P/main.py
+++ 10.1P/main.py
@@ -1,14 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from clock import Clock
 
 
 def main():
-    print("Hello")
 
-# Press the green button in the gutter to run the script.
+    clock = Clock()
+
+    for x in range(86400):
+        clock.tick()
+
+    print(clock.time)
+
+
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
